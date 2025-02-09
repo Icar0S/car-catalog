50f33ed7bd4c6a1bc2cf852c301a2095705491b4
Icar0S <icarooliveira_23@hotmail.com>
2025-02-09 14:16:44 -0300

    fix path folder

--- js/data/carCatalog.js
+++ js/data/carCatalog.js
@@ -3,56 +3,56 @@ export const carCatalog = [
     {
         brand: "Audi",
         vehicles: [
-            { model: "A3", image: "../../images/audi/audi-a3.png" },
-            { model: "A4", image: "../../images/audi/audi-a4.png" },
-            { model: "A5", image: "../../images/audi/audi-a5.png" },
-            { model: "A6", image: "../../images/audi/audi-a6.png" },
-            { model: "R8", image: "../../images/audi/audi-r8.png" }
+            { model: "A3", image: "./images/audi/audi-a3.png" },
+            { model: "A4", image: "./images/audi/audi-a4.png" },
+            { model: "A5", image: "./images/audi/audi-a5.png" },
+            { model: "A6", image: "./images/audi/audi-a6.png" },
+            { model: "R8", image: "./images/audi/audi-r8.png" }
         ]
     },
     {
         brand: "BMW",
         vehicles: [
-            { model: "M3", image: "../../images/bmw/bmw-m3.png" },
-            { model: "M4", image: "../../images/bmw/bmw-m4.png" },
-            { model: "X1", image: "../../images/bmw/bmw-x1.png" },
-            { model: "X6", image: "../../images/bmw/bmw-x6.png" },
-            { model: "iX", image: "../../images/bmw/bmw-ix.png" },
-            { model: "I7", image: "../../images/bmw/bmw-i7.png" }
+            { model: "M3", image: "./images/bmw/bmw-m3.png" },
+            { model: "M4", image: "./images/bmw/bmw-m4.png" },
+            { model: "X1", image: "./images/bmw/bmw-x1.png" },
+            { model: "X6", image: "./images/bmw/bmw-x6.png" },
+            { model: "iX", image: "./images/bmw/bmw-ix.png" },
+            { model: "I7", image: "./images/bmw/bmw-i7.png" }
         ]
     },
     {
         brand: "Ferrari",
         vehicles: [
-            { model: "F8 Tributo", image: "../../images/ferrari/ferrari-f8.png" },
-            { model: "SF90", image: "../../images/ferrari/ferrari-sf90.png" },
-            { model: "296 GTB", image: "../../images/ferrari/ferrari-296.png" },
-            { model: "812 Superfast", image: "../../images/ferrari/ferrari-812.png" },
-            { model: "488 Pista", image: "../../images/ferrari/ferrari-488.png" },
-            { model: "Purosangue", image: "../../images/ferrari/ferrari-purosangue.png" }
+            { model: "F8 Tributo", image: "./images/ferrari/ferrari-f8.png" },
+            { model: "SF90", image: "./images/ferrari/ferrari-sf90.png" },
+            { model: "296 GTB", image: "./images/ferrari/ferrari-296.png" },
+            { model: "812 Superfast", image: "./images/ferrari/ferrari-812.png" },
+            { model: "488 Pista", image: "./images/ferrari/ferrari-488.png" },
+            { model: "Purosangue", image: "./images/ferrari/ferrari-purosangue.png" }
         ]
     },
     {
         brand: "Fiat",
         vehicles: [
-            { model: "Pulse", image: "../../images/fiat/fiat-pulse.png" },
-            { model: "Fastback", image: "../../images/fiat/fiat-fastback.png" },
-            { model: "500", image: "../../images/fiat/fiat-500.png" },
-            { model: "Punto", image: "../../images/fiat/fiat-punto.png" },
-            { model: "Toro", image: "../../images/fiat/fiat-toro.png" }
+            { model: "Pulse", image: "./images/fiat/fiat-pulse.png" },
+            { model: "Fastback", image: "./images/fiat/fiat-fastback.png" },
+            { model: "500", image: "./images/fiat/fiat-500.png" },
+            { model: "Punto", image: "./images/fiat/fiat-punto.png" },
+            { model: "Toro", image: "./images/fiat/fiat-toro.png" }
         ]
     },
     {
         brand: "Ford",
         vehicles: [
-            { model: "Mustang", image: "../../images/ford/ford-Mustang.png" },
-            { model: "Mustang GT500", image: "../../images/ford/ford-eleanor-mustang.png" },
-            { model: "Raptor", image: "../../images/ford/ford-Raptor.avif" },
+            { model: "Mustang", image: "./images/ford/ford-Mustang.png" },
+            { model: "Mustang GT500", image: "./images/ford/ford-eleanor-mustang.png" },
+            { model: "Raptor", image: "./images/ford/ford-Raptor.avif" },
             { 
               model: "F-150", 
               images: [
-                  "../../images/ford/ford-f150.png",
-                  "../../images/ford/ford-f150-2.png"
+                  "./images/ford/ford-f150.png",
+                  "./images/ford/ford-f150-2.png"
               ],
               currentImageIndex: 0
             }
@@ -64,80 +64,80 @@ export const carCatalog = [
             { 
               model: "Accord", 
               images: [
-                  "../../images/honda/honda-accord.png",
-                  "../../images/honda/honda-accord-gray.png"
+                  "./images/honda/honda-accord.png",
+                  "./images/honda/honda-accord-gray.png"
               ],
               currentImageIndex: 0
             },
             { 
               model: "Civic", 
               images: [
-                  "../../images/honda/honda-civic-red.png",
-                  "../../images/honda/honda-civic-black.png"
+                  "./images/honda/honda-civic-red.png",
+                  "./images/honda/honda-civic-black.png"
               ],
               currentImageIndex: 0
             },
             { 
               model: "Civic Si", 
               images: [
-                  "../../images/honda/honda-civic-si.png",
-                  "../../images/honda/honda-civic.png"
+                  "./images/honda/honda-civic-si.png",
+                  "./images/honda/honda-civic.png"
               ],
               currentImageIndex: 0
             },
             { 
               model: "Civic Type R", 
               images: [
-                  "../../images/honda/honda-civic-type-r.png",
-                  "../../images/honda/honda-civic-type-r-1.png"
+                  "./images/honda/honda-civic-type-r.png",
+                  "./images/honda/honda-civic-type-r-1.png"
               ],
               currentImageIndex: 0
             },
-            { model: "CR-V", image: "../../images/honda/honda-crv.png" }
+            { model: "CR-V", image: "./images/honda/honda-crv.png" }
         ]
     },
     {
         brand: "Hyundai",
         vehicles: [
-            { model: "HB20", image: "../../images/hyundai/hyundai-hb20.png" },
-            { model: "Creta", image: "../../images/hyundai/hyundai-creta.png" },
-            { model: "Santa Fe", image: "../../images/hyundai/hyundai-santafe.png" }
+            { model: "HB20", image: "./images/hyundai/hyundai-hb20.png" },
+            { model: "Creta", image: "./images/hyundai/hyundai-creta.png" },
+            { model: "Santa Fe", image: "./images/hyundai/hyundai-santafe.png" }
         ]
     },
     {
         brand: "Lamborghini",
         vehicles: [
-            { model: "Huracán", image: "../../images/lambo/lamborghini-huracan.png" },
-            { model: "Aventador", image: "../../images/lambo/lamborghini-aventador.png" },
-            { model: "Urus", image: "../../images/lambo/lamborghini-urus.png" }
+            { model: "Huracán", image: "./images/lambo/lamborghini-huracan.png" },
+            { model: "Aventador", image: "./images/lambo/lamborghini-aventador.png" },
+            { model: "Urus", image: "./images/lambo/lamborghini-urus.png" }
         ]
     },
     {
         brand: "Mercedes",
         vehicles: [
-            { model: "AMG GT", image: "../../images/mercedes/mercedes-amg.png" },
-            { model: "Classe C", image: "../../images/mercedes/mercedes-c-class.png" },
-            { model: "GLA", image: "../../images/mercedes/mercedes-gla.png" }
+            { model: "AMG GT", image: "./images/mercedes/mercedes-amg.png" },
+            { model: "Classe C", image: "./images/mercedes/mercedes-c-class.png" },
+            { model: "GLA", image: "./images/mercedes/mercedes-gla.png" }
         ]
     },
     {
         brand: "Porsche",
         vehicles: [
-            { model: "911", image: "../../images/porsche/porsche-911.png" },
-            { model: "918", image: "../../images/porsche/porsche-918.png" },
-            { model: "Panamera", image: "../../images/porsche/porsche-panamera.png" },
-            { model: "Taycan", image: "../../images/porsche/porsche-taycan.png" }
+            { model: "911", image: "./images/porsche/porsche-911.png" },
+            { model: "918", image: "./images/porsche/porsche-918.png" },
+            { model: "Panamera", image: "./images/porsche/porsche-panamera.png" },
+            { model: "Taycan", image: "./images/porsche/porsche-taycan.png" }
         ]
     },
     {
         brand: "Toyota",
         vehicles: [
-            { model: "Corolla", image: "../../images/toyota/toyota-corolla.png" },
-            { model: "GR Corolla", image: "../../images/toyota/toyota-gr-corolla.png" },
-            { model: "Hilux GR Sport 2024", image: "../../images/toyota/toyota-gr-hilux.png" },
-            { model: "Hilux", image: "../../images/toyota/toyota-hilux.png" },
-            { model: "Supra", image: "../../images/toyota/toyota-supra.png" },
-            { model: "Supra Film", image: "../../images/toyota/toyota-supra-film.png" }
+            { model: "Corolla", image: "./images/toyota/toyota-corolla.png" },
+            { model: "GR Corolla", image: "./images/toyota/toyota-gr-corolla.png" },
+            { model: "Hilux GR Sport 2024", image: "./images/toyota/toyota-gr-hilux.png" },
+            { model: "Hilux", image: "./images/toyota/toyota-hilux.png" },
+            { model: "Supra", image: "./images/toyota/toyota-supra.png" },
+            { model: "Supra Film", image: "./images/toyota/toyota-supra-film.png" }
         ]
     },
     {
@@ -146,13 +146,13 @@ export const carCatalog = [
             { 
                 model: "Golf GTI", 
                 images: [
-                    "../../images/volkswagen/vw-golf-gti.png",
-                    "../../images/volkswagen/vw-golf-gti-1.png"
+                    "./images/volkswagen/vw-golf-gti.png",
+                    "./images/volkswagen/vw-golf-gti-1.png"
                 ],
                 currentImageIndex: 0
             },
-            { model: "Jetta", image: "../../images/volkswagen/vw-jetta.png" },
-            { model: "Tiguan", image: "../../images/volkswagen/vw-tiguan.png" }
+            { model: "Jetta", image: "./images/volkswagen/vw-jetta.png" },
+            { model: "Tiguan", image: "./images/volkswagen/vw-tiguan.png" }
         ]
     }
 ];
